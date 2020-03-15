55fdf9b2fe7467143c1dfeaf0c59a47ed2e9170c
jordanStephenson <jordan_535@hotmail.com>
2020-03-15 20:35:29 +0000

    Full screen slider added + theme change

--- script.js
+++ script.js
@@ -32,7 +32,68 @@ const navSlide = () => {
 }
 navSlide();
 
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
 
+
+
+const nextSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for next slide
+    if(current.nextElementSibling) {
+        //Add current to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        //Add current to start
+        slides[0].classList.add('current');
+    }     
+    setTimeout(() => current.classList.remove('current')); 
+}
+
+const prevSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for previous slide
+    if(current.previousElementSibling) {
+        //Add current to previous sibling
+        current.previousElementSibling.classList.add('current');
+    } else {
+        //Add current to last
+        slides[slides.length -1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+};
+
+// Button events
+next.addEventListener('click', e => {
+    nextSlide();
+    if(auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+prev.addEventListener('click', e => {
+    prevSlide();
+    if(auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+
+// Auto slide
+if(auto) {
+    // Run next slide at interval time
+    slideInterval = setInterval(nextSlide, intervalTime);
+}
 
 /* Timeline animations*/
 
@@ -48,16 +109,11 @@ tl.from('.landing-page h2', 2, {
     y: 10,
     ease: Power2.easeOut
 }, .5) ;
-tl.from('.scroll', 2, {
+tl.from('.button', 2, {
     opacity: 0,
     y: 10,
     ease: Power2.easeOut
-}, 1) ;
-tl.from('.profile-pic', 2, {
-    opacity: 0,
-    y: 20,
-    ease: Power2.easeOut
-}, 0);
+}, 1);
 
 
 
@@ -103,7 +159,7 @@ if(galleryImages) {
         image.addEventListener('click', () => {
             let getElementCss = window.getComputedStyle(image);
             let getFullImgUrl = getElementCss.getPropertyValue('background-image');
-            let getImgUrlPos = getFullImgUrl.split('IMG/thumb');
+            let getImgUrlPos = getFullImgUrl.split('IMG/gallery');
             let setNewImgUrl = getImgUrlPos[1].replace('")', '');
 
             getLatestOpenedImg = index + 1;
